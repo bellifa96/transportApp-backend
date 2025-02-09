d6a8804ac0ce93207d010fbe808fcdc389efb172
bellifa96 <bellifa96@live.fr>
2025-02-09 13:34:45 +0100

    register return token

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,10 @@ const { User } = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-// Fonction pour créer un utilisateur
+
+// Fonction pour créer un utilisateur et le connecter immédiatement
 const createUser = async (req, res) => {
-  const { email, password, firstName, lastName, phone, profilePhoto,role } = req.body;
+  const { email, password, firstName, lastName, phone, profilePhoto, role } = req.body;
 
   try {
     // Vérifier si l'email existe déjà
@@ -14,18 +15,41 @@ const createUser = async (req, res) => {
       return res.status(400).json({ message: 'User with this email already exists' });
     }
 
-    // Créer le nouvel utilisateur
+    // Hachage du mot de passe avant la sauvegarde
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Création du nouvel utilisateur
     const newUser = await User.create({
       email,
-      password,
+      password: hashedPassword, // Stocker le mot de passe haché
       firstName,
       lastName,
       phone,
       profilePhoto,
-      role: role || 'client', // Définit le rôle par défaut à 'client' si aucun n'est fourni
+      role: role || 'client',
     });
 
-    res.status(201).json({ message: 'User created successfully', user: newUser });
+    // Génération du token JWT
+    const token = jwt.sign(
+      { id: newUser.id, email: newUser.email, role: newUser.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    // Retourne les mêmes données qu'un login
+    res.status(201).json({
+      message: 'User created and logged in successfully',
+      token,
+      user: {
+        id: newUser.id,
+        email: newUser.email,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        phone: newUser.phone,
+        profilePhoto: newUser.profilePhoto,
+        role: newUser.role,
+      },
+    });
   } catch (error) {
     res.status(500).json({ message: 'Server error', error });
   }
@@ -62,6 +86,8 @@ const login = async (req, res) => {
         firstName: user.firstName,
         lastName: user.lastName,
         role:user.role,
+        phone: user.phone,
+        profilePhoto: user.profilePhoto,
       },
     });
   } catch (error) {
